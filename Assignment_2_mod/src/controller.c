7bdc7b2ca18a3189f1fdcc1dde0deab65f83bc52
Andrea <abadillo@mit.edu>
2020-04-07 15:51:35 -0400

    working except for waypoints

--- Assignment_2_mod/src/controller.c
+++ Assignment_2_mod/src/controller.c
@@ -3,165 +3,130 @@
 #include <math.h>
 #include<stdio.h>
 
-double check_pos_bounds(double val){
-    printf("checking pos bounds\n");
-    fflush(stdout);
-    if(val < 0){
-        printf("will be zero\n");
-        fflush(stdout);
-        return 0;
-    }
-    if(val >=100){
-        printf("will be 99.9\n");
-        fflush(stdout);
-        return 99.9;
-    }
-    printf("will pass value pos\n");
-    fflush(stdout);
-    return val;
-}
+double check_angle(double val){
+    // printf("checking head bounds\n");
+    // fflush(stdout);
 
-double check_head_bounds(double val){
-    printf("checking head bounds\n");
-    fflush(stdout);
-    if(val < -M_PI){
-        printf("will be lower bound\n");
-        fflush(stdout);
-        return -M_PI;
+	//either returns original theta if within bounds or will
+	//simplify theta to be within bounds
+
+	while(val < 0.0){
+		val += 2*M_PI;
+	}
+
+	//pass through theta if already bounded
+    if((val >= -M_PI) && (val < M_PI)){
+        return val;
     }
 
-    if(val >= M_PI){
-        printf("will be higher bound\n");
-        fflush(stdout);
-        return 179*M_PI/180;
+   //get equivalent theta in 0 to 2pi if not bounded
+    val = fmod(val,(2*M_PI));
+
+    //check if less than pi, if not, make a neg angle (will always be between -pi and pi now)
+    if (val < M_PI)
+    {
+    	return val;
     }
-    printf("pass value head\n");
-    fflush(stdout);
-    return val;
+
+    return val - 2*M_PI;
+  
+
 }
 
 double check_linv_bounds(double val){
-    printf("checking linv bounds\n");
-    fflush(stdout);
+    // printf("checking linv bounds\n");
+    // fflush(stdout);
     if(val < 5){
-        printf("will be lower bound\n");
-        fflush(stdout);
+        // printf("will be lower bound\n");
+        // fflush(stdout);
         return 5;
     }
     if(val > 10){
-        printf("will be higher bound\n");
-        fflush(stdout);
+        // printf("will be higher bound\n");
+        // fflush(stdout);
         return 10;
     }
-    printf("pass value linv\n");
-    fflush(stdout);
+    // printf("pass value linv\n");
+    // fflush(stdout);
     return val;
 }
 
 double check_angv_bounds(double val){
-    printf("checking angv bounds\n");
-    fflush(stdout);
+    // printf("checking angv bounds\n");
+    // fflush(stdout);
     if(val < -M_PI_4){
-        printf("will be lower bounds\n");
-        fflush(stdout);
+        // printf("will be lower bounds\n");
+        // fflush(stdout);
         return -M_PI_4;
     }
     if(val > M_PI_4){
-        printf("will be higher bound\n");
-        fflush(stdout);
+        // printf("will be higher bound\n");
+        // fflush(stdout);
         return M_PI_4;
     }
-    printf("pass value angv\n");
-    fflush(stdout);
+    // printf("pass value angv\n");
+    // fflush(stdout);
     return val;
 }
 
+
+
 control get_proportional_waypoint_control(struct t_vehicle * vehicle){
-	printf("initializing controller \n");
-    fflush(stdout);
-    control * ctrl = malloc(sizeof(control));
-    printf("allocated space to controller, about to get pos\n");
+    printf("getting control\n");
     fflush(stdout);
     
-    /*
+    control * ctrl = malloc(sizeof(control));
+    
     //pos_v is position of vehicle and pos_w is position of waypoint
     double pos_vx = (*vehicle).position[0];
     double pos_vy = (*vehicle).position[1];
-    printf("got pos vehicle\n");
-    printf("%f%f\n",(pos_vx,pos_vy));
-    fflush(stdout);
-    
+        
     double pos_wx = (*vehicle).current_waypoint[0];
     double pos_wy = (*vehicle).current_waypoint[1]; 
-    printf("got pos waypoint\n");
-    printf("%f%f\n", (pos_wx,pos_wy));
-    fflush(stdout);
-
-    //---------------------------------------------------------------
-    //checking if waypoint needs to be updated and if vehicle has 
-    //passed through all the waypoints
+    
     double dist = sqrt(pow(pos_wx-pos_vx,2) + pow(pos_wy-pos_vy,2));
-    printf("calculated distance\n");
-    fflush(stdout);
-    if (dist < 0.5)
-    {
-        printf("increasing index\n");
-        fflush(stdout);
-        (*vehicle).current_waypoint_idx++;
-
-        if((*vehicle).current_waypoint_idx < (*vehicle).num_waypoints){
-        printf("updating current waypoint to match next one\n");
-        fflush(stdout);
-        (*vehicle).current_waypoint[0] = (*vehicle).target_waypoints[(*vehicle).current_waypoint_idx][0];
-        (*vehicle).current_waypoint[1] = (*vehicle).target_waypoints[(*vehicle).current_waypoint_idx][1];
-        printf("successfully increased waypoint\n");
-        fflush(stdout);
-        }
-
-        else{
-            (*ctrl).speed = 0;
-            (*ctrl).angular_velocity = 0;
-            return *ctrl;
-        }
-    }
-
+   
     //---------------------------------------------------------------
-	//getting angle between vehicle heading and waypoint
-    printf("getting ang vehicle\n");
-    fflush(stdout);
-    double ang_v = (*vehicle).position[2];
-    printf("successfully got ang vehicle\n");
-    fflush(stdout);
-    double ang_w  = tan(pos_wy/pos_wx); //angle of the waypoint
+	//getitng angle difference relative to vehicle and waypoint
+    double ang_v = (*vehicle).position[2]; //angle of vehicle heading
+    double ang_g  = atan2((pos_wy-pos_vy),(pos_wx-pos_vx)); //goal angle
     
+    //since abs(angle diff) must be less than pi, make this happen
     double angle_diff;
-    
-    double v_w = ang_v - ang_w;
-    double w_v = ang_w - ang_v;
-    
-    if (abs(v_w) < abs(w_v)){
-        angle_diff = v_w;
+
+    while(ang_v < 0){
+    	ang_v += 2*M_PI;
     }
-    else{
-        angle_diff = w_v;
+    while(ang_g < 0){
+    	ang_g += 2*M_PI;
     }
-    
+
+    //makes sure that angle difference is within neg pi and pi
+    angle_diff = fmod((ang_g-ang_v),2*M_PI);
+    angle_diff = check_angle(angle_diff);
+        
     //---------------------------------------------------------------
 	//controllers
 
 	//speed prop controller based on distance error
     double speed = check_linv_bounds(dist); 
-
+    
 	//angular controller based off of specifications
 	double ang_vel = check_angv_bounds(angle_diff);
-	*/
-
+	
     //for debugging purposes
-    double speed = 6;
-    double ang_vel = M_PI/6;
+    // double speed = 6;
+    // double ang_vel = M_PI/6;
 
 	(*ctrl).speed = speed;
-	(*ctrl).angular_velocity = ang_vel; 
+	(*ctrl).angular_velocity = ang_vel;
+
+	printf("speed: %f\n", (*ctrl).speed);
+    fflush(stdout);
+    
+    printf("ang_vel: %f \n", (*ctrl).angular_velocity);
+	fflush(stdout);
+    
     printf("returning control\n");
     fflush(stdout);
 	return *ctrl;
--- Assignment_2_mod/src/vehicle.c
+++ Assignment_2_mod/src/vehicle.c
@@ -7,6 +7,49 @@
 #include <unistd.h>
 #include <stdio.h>
 
+double check_pos_bounds(double val){
+    // printf("checking pos bounds\n");
+    // fflush(stdout);
+    if(val < 0){
+        val = 0.0;
+    }
+    if(val >=100.0){
+        val =  99.9999;
+    }
+    printf("value at pos_bounds: %f\n", val);
+    fflush(stdout);
+    return val;
+}
+
+double check_head_bounds(double val){
+    // printf("checking head bounds\n");
+    // fflush(stdout);
+
+	//either returns original theta if within bounds or will
+	//simplify theta to be within bounds
+
+	while(val < 0.0){
+		val += 2*M_PI;
+	}
+
+	//pass through theta if already bounded
+    if((val >= -M_PI) && (val < M_PI)){
+        return val;
+    }
+
+   //get equivalent theta in 0 to 2pi if not bounded
+    val = fmod(val,(2*M_PI));
+
+    //check if less than pi, if not, make a neg angle (will always be between -pi and pi now)
+    if (val < M_PI)
+    {
+    	return val;
+    }
+
+    return val - 2*M_PI;
+  
+
+}
 
 void set_position(struct t_vehicle * v,double * values){
     /*
@@ -14,16 +57,15 @@ void set_position(struct t_vehicle * v,double * values){
     not exceed [0,100)
     heading limited to [-pi,pi)
     */
-
     printf("calling set position\n");
     fflush(stdout);
     for (int i = 0; i < 3; ++i)
     {
-        printf("in for loop set POSITION, iteration %d\n", i);
-        fflush(stdout);
+    	printf("position value: %f, idx %d\n", values[i],i );
+    	fflush(stdout);
         (*v).position[i] = values[i];
     }
-    printf("set position\n");
+    printf("x pos: %f, y pos %f, theta = %f\n", (*v).position[0],(*v).position[1], (*v).position[2]);
     fflush(stdout);
 }
 
@@ -38,14 +80,16 @@ void set_velocity(struct t_vehicle * v, double * values){
     fflush(stdout);
     for (int i = 0; i < 3; ++i)
     {
-        printf("in for loop set velocity, iteration %d\n", i);
-        fflush(stdout);
+        // printf("in for loop set velocity, iteration %d\n", i);
+        // fflush(stdout);
 
         (*v).velocity[i] = values[i];
         
-        printf("set velocity\n");
-        fflush(stdout);
+        // printf("set velocity\n");
+        // fflush(stdout);
     }
+    printf("x vel: %f, y vel: %f, theta = %f\n", (*v).velocity[0],(*v).velocity[1], (*v).velocity[2]);
+    fflush(stdout);
 }
 
 
@@ -56,20 +100,26 @@ void control_vehicle(struct t_vehicle * v){
     printf("calling control vehicle\n");
     fflush(stdout);
 
-
     control ctrl = get_proportional_waypoint_control(v);
-
-    printf("control successfully called\n");
+    
+    printf("control successfully called: speed = %f, angvel = %f\n", ctrl.speed, ctrl.angular_velocity);
     fflush(stdout);
-
+    
     double th = (*v).position[2];
     double x_vel = ctrl.speed * cos(th);
     double y_vel = ctrl.speed * sin(th);
-
-    printf("linear velocities set\n");
-    fflush(stdout);
+    double ang_vel = ctrl.angular_velocity;
     
-    double values[3] = {x_vel, y_vel, ctrl.angular_velocity};
+
+    printf("variables x vel: %f, y vel: %f, th vel = %f\n", x_vel,y_vel, ang_vel);
+    fflush(stdout);
+
+    // printf("linear velocities set\n");
+    // fflush(stdout);
+    double values[3] = {x_vel, y_vel, ang_vel};
+
+    printf("array x vel: %f, y vel: %f, th vel = %f\n", values[0],values[1], values[2]);
+    fflush(stdout);
 
     set_velocity(v, values);
 }
@@ -81,36 +131,80 @@ void update_state(struct t_vehicle * v, double delta_t){
     y += y_dot*delta_t
     th += th_dot*delta_t
     */
-    printf("update state\n");
-    fflush(stdout);
+    // printf("update state\n");
+    // fflush(stdout);
+
     double temp;
     double update;
-
+    double vals[3];
+   
     for (int i = 0; i <3; ++i)
-    {   printf("in for loop update state, iteration %d\n", i);
+    {   
+    	// printf("in for loop update state, iteration %d\n", i);
+    //     fflush(stdout);
+        
+        temp = (*v).position[i] + (*v).velocity[i]*delta_t;
+        printf("updating state: %f\n", temp);
+        fflush(stdout);
+        // printf("got temp variable\n");
+        // fflush(stdout);
+        
+        if (i<2){
+            update = check_pos_bounds(temp);
+            printf("update value x_y: %f\n", update);
+        	fflush(stdout);
+        }
+        else{
+            update = check_head_bounds(temp);
+            printf("update value th: %f\n", update);
+        	fflush(stdout);
+        }
+        
+        // printf("about to update state\n");
+        // fflush(stdout);
+        
+        vals[i] = update;
+        printf("did array update properly? %f\n", vals[i]);
+        fflush(stdout);
+        // printf("updated state\n");
+        // fflush(stdout);
+    }
+    set_position(v, vals);
+
+    //updating waypoint maybe
+    double dist = sqrt(pow((*v).current_waypoint[0]-(*v).position[0],2) + pow((*v).current_waypoint[1]-(*v).current_waypoint[1],2));
+    
+    printf("calculated distance, %f\n", dist);
+    fflush(stdout);
+    
+    if (dist < 0.5)
+    {
+        printf("increasing index\n");
         fflush(stdout);
 
-        temp = (*v).position[i] + (*v).velocity[i]*delta_t;
-        
-        printf("got temp variable\n");
+        int num = (*v).current_waypoint_idx + 1;
+        (*v).current_waypoint_idx = num%((*v).num_waypoints);
+
+        printf("updating current waypoint to match next one\n");
         fflush(stdout);
+
+        (*v).current_waypoint[0] = (*v).target_waypoints[(*v).current_waypoint_idx][0];
+        (*v).current_waypoint[1] = (*v).target_waypoints[(*v).current_waypoint_idx][1];
         
-        printf("about to update state\n");
-        fflush(stdout);
-        
-        (*v).position[i] = update;
-        
-        printf("updated state\n");
+        printf("successfully increased waypoint\n");
         fflush(stdout);
     }
+
 }
 
 
 vehicle * create_vehicle(double * starting_pos, int n_waypoints, double ** offset_waypoints){
     // initialization
+    // printf("initializing vehicle\n");
+    // fflush(stdout);
     vehicle * v = malloc(sizeof(vehicle));
-    
-
+    // printf("allocated vehicle memory\n");
+    // fflush(stdout);
     //set starting position on vehicle making sure that the starting position is within the bounds specified
     
     //initialize function pointers
@@ -120,55 +214,68 @@ vehicle * create_vehicle(double * starting_pos, int n_waypoints, double ** offse
     (*v).update_state = &update_state;
 
 
-    double pos_x = check_pos_bounds(starting_pos[0]);
-    double pos_y = check_pos_bounds(starting_pos[1]);
-    double th = check_head_bounds(starting_pos[2]);
-    printf("accessed positions create vehicle\n");
-    fflush(stdout);
+    (*v).position[0] = check_pos_bounds(starting_pos[0]);
+    (*v).position[1] = check_pos_bounds(starting_pos[1]);
+    (*v).position[2] = check_head_bounds(starting_pos[2]);
 
-    double pos[3] = {pos_x, pos_y, th};
-    set_position(v, pos);
-    printf("set positions create vehicle\n");
+    printf("x pos: %f, y pos %f, theta = %f\n", ((*v).position[0],(*v).position[1], (*v).position[2]));
     fflush(stdout);
+    // printf("accessed positions create vehicle\n");
+    // fflush(stdout);
+
+    //set initial vel to min values and ang_vel 0, let next update control
+    (*v).velocity[0] = 6.0;
+    (*v).velocity[1] = 6.0;
+    (*v).velocity[2] = M_PI/5;
+
+    // printf("set positions create vehicle\n");
+    // fflush(stdout);
 
     //initializing target waypoints to be rel to world frame rather than starting pos
     (*v).num_waypoints = n_waypoints;
     (*v).current_waypoint_idx = 0;
-    (*v).current_waypoint = malloc(2*sizeof(double));
+
+    
 
     (*v).target_waypoints = malloc(((*v).num_waypoints) * sizeof(double*));
 
-    for (int i = 0; i < n_waypoints+1; i++) {
-        printf("in for loop target waypoints, iteration %d\n",i);
-        fflush(stdout);
+    for (int i = 0; i < n_waypoints; i++) {
+        // printf("in for loop target waypoints, iteration %d\n",i);
+        // fflush(stdout);
+
         (*v).target_waypoints[i] = malloc(2 * sizeof(double));
-        (*v).target_waypoints[i][0] = check_pos_bounds(offset_waypoints[i][0] + (*v).position[0]);
-        (*v).target_waypoints[i][1] = check_pos_bounds(offset_waypoints[i][1] + (*v).position[1]);
-        printf("set target waypoints, iteration %d\n",i);
+        (*v).target_waypoints[i][0] = fmod(fmod(offset_waypoints[i][0] + (*v).position[0],100)+100, 100);
+        printf("waypoint %d x pos is %f\n",(i,(*v).target_waypoints[i][0]));
         fflush(stdout);
-        if(i == (*v).current_waypoint_idx){
-            printf("setting initial waypoints\n");
-            fflush(stdout);
-            (*v).current_waypoint[0] = (*v).target_waypoints[i][0];
-            (*v).current_waypoint[1] = (*v).target_waypoints[i][1];
-            printf("initial waypoints set\n");
-            fflush(stdout);
+
+        (*v).target_waypoints[i][1] = fmod(fmod(offset_waypoints[i][1] + (*v).position[1],100)+100, 100);
+        printf("waypoint %d y pos is %f\n",(i,(*v).target_waypoints[i][1]));
+        fflush(stdout);
+        // printf("set target waypoints, iteration %d\n",i);
+        // fflush(stdout);
         }
+    (*v).current_waypoint = malloc(2*sizeof(double));
+    (*v).current_waypoint = (*v).target_waypoints[0];
+    return v;
+
     }
 
-    //set initial vel using controller
-    control * init = malloc(sizeof(control));
-    printf("getting prop control initial\n");
-    fflush(stdout);
-    *init = get_proportional_waypoint_control(v);
-    printf("prop control achieved, setting values\n");
-    fflush(stdout);
-    double x_vel = (*init).speed * cos(th);
-    double y_vel = (*init).speed * sin(th);
-    double vel[3] = {x_vel, y_vel, (*init).angular_velocity};
-    printf("values set\n");
-    fflush(stdout);
-    set_velocity(v, vel);
+    
 
-    return v;
-}
+
+    // control * init = malloc(sizeof(control));
+    // *init = get_proportional_waypoint_control(v);
+    
+    // printf("prop control achieved, setting values\n");
+    // fflush(stdout);
+    
+    // double x_vel = (*init).speed * cos(th);
+    // double y_vel = (*init).speed * sin(th);
+    // double vel[3] = {x_vel, y_vel, (*init).angular_velocity};
+    
+    // printf("values set\n");
+    // fflush(stdout);
+    
+    // set_velocity(v, vel);
+
+    
